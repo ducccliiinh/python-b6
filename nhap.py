482f839f02e43799d21c5dd3bd2e483d80243203
ducccliiinh <164066152+ducccliiinh@users.noreply.github.com>
2024-03-31 14:41:21 +0700

    create new file nhap.py

---
+++ nhap.py
@@ -0,0 +1,23 @@
+# def prime(a):
+#     check = False
+#     if a > 0:
+#         if a == 1:
+#             check = False
+#         for i in range(2, int(a**0.5) + 1):
+#             if a % i == 0:
+#                 check = False
+#                 break
+#             else:
+#                 check = True
+#     return check
+            
+# n = int(input())
+# count = 0
+# for i in range(1, n + 1):
+#     if prime(i):
+#         count += 1
+# print(count)
+
+print("Vừng ơi, ..... mở ra")
+pw = input("Nhập mật khẩu để mở ứng dụng:")
+print("Xin chào" if pw == "python" else "Nhập lại mật khẩu")
